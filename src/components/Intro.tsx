4c7b2cdb955e35ab2be42c9ab5ac8f212e5988cb
anjuman raj <ayusmann23@gmail.com>
2022-08-22 03:14:25 +0530

    resume link updated

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -29,8 +29,12 @@ const Intro: FC = () => {
       </m.p>
  
       <div className="flex flex-wrap">
-      <m.a href="#who" className="text-[#FC6459] hover:text-dark border border-[#FC6459] hover:bg-[#FC6459] cursor-pointer font-medium rounded-lg text-lg px-5 py-2 text-center mr-2 mb-2  z-50 transition-all duration-150 ease-linear hover:scale-105  ">Résumé</m.a>
+      <m.a href="anjuman-Resume.pdf" className="text-[#FC6459] hover:text-dark border border-[#FC6459] hover:bg-[#FC6459] cursor-pointer font-medium rounded-lg text-lg px-5 py-2 text-center mr-2 mb-2  z-50 transition-all duration-150 ease-linear hover:scale-105  ">Résumé</m.a>
+
+
       <m.a href="https://www.linkedin.com/in/anjuman-raj-ba390b1b5/" className="text-[#0077B5] hover:text-dark border border-[#0077B5] hover:bg-[#0077B5] cursor-pointer font-medium rounded-lg text-lg  px-5 py-2 text-center mr-2 mb-2  z-50 transition-all duration-150 ease-linear hover:scale-105  ">LinkedIn</m.a>
+
+
       <m.a href="Github.com/ayusmann" className="text-[#ffffff] hover:text-dark border border-[#fff] hover:bg-white hover:border-none cursor-pointer font-medium rounded-lg text-lg px-5 py-2 text-center mr-2 mb-2  z-50 transition-all duration-150 ease-linear hover:scale-105 ">Github</m.a>
       </div>
 
